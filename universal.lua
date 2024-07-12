ed55e3b55a888d4953dab1bbf515a8718471f332
zyo <145725708+qu1ver@users.noreply.github.com>
2024-07-13 11:24:52 +1200

    Update universal.lua
    added line esp and hitboxes

--- universal.lua
+++ universal.lua
@@ -1,12 +1,13 @@
 local uiColor = Color3.fromRGB(255, 0, 0)
 
+local lineESP = false
 local aimbotColor = Color3.fromRGB(255, 0, 0)
 local espColor = Color3.fromRGB(255, 0, 0)
 local walkspeedNum = 16
 
 local lplr = game.Players.LocalPlayer
-local camera = workspace.CurrentCamera
-local worldToViewportPoint = camera.WorldToViewportPoint
+local camera = game:GetService("Workspace").CurrentCamera
+local worldToViewportPoint = camera.worldToViewportPoint
 
 local HeadOff = Vector3.new(0, 0.5, 0)
 local LegOff = Vector3.new(0, 1.5, 0)
@@ -16,13 +17,6 @@ local boxEnabled = false
 local teamCheck = false
 
 function createBox()
-    local BoxOutline = Drawing.new("Square")
-    BoxOutline.Visible = false
-    BoxOutline.Color = Color3.new(0, 0, 0)
-    BoxOutline.Thickness = 3
-    BoxOutline.Transparency = 1
-    BoxOutline.Filled = false
-
     local Box = Drawing.new("Square")
     Box.Visible = false
     Box.Color = espColor
@@ -30,18 +24,18 @@ function createBox()
     Box.Transparency = 1
     Box.Filled = false
 
-    return BoxOutline, Box
+    return Box
 end
 
-function boxesp(v)
+local function boxesp(v)
     if not boxes[v] then
-        boxes[v] = {createBox()}
+        boxes[v] = { createBox() }
     end
-    local BoxOutline, Box = unpack(boxes[v])
+    local Box = boxes[v][1]
 
     local connection
     connection = game:GetService("RunService").RenderStepped:Connect(function()
-        if v.Character and v.Character:FindFirstChild("Humanoid") and v.Character:FindFirstChild("HumanoidRootPart") and v ~= lplr and v.Character.Humanoid.Health > 0 then
+        if v.Character and v.Character:FindFirstChild("HumanoidRootPart") and v ~= lplr and v.Character.Humanoid.Health > 0 then
             local RootPart = v.Character.HumanoidRootPart
             local Head = v.Character.Head
             local RootPosition, RootVis = worldToViewportPoint(camera, RootPart.Position)
@@ -49,26 +43,19 @@ function boxesp(v)
             local LegPosition = worldToViewportPoint(camera, RootPart.Position - LegOff)
 
             if RootVis and boxEnabled then
-                BoxOutline.Size = Vector2.new(1000 / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
-                BoxOutline.Position = Vector2.new(RootPosition.X - BoxOutline.Size.X / 2, RootPosition.Y - BoxOutline.Size.Y / 2)
-                BoxOutline.Visible = true
-
                 Box.Size = Vector2.new(1000 / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
                 Box.Position = Vector2.new(RootPosition.X - Box.Size.X / 2, RootPosition.Y - Box.Size.Y / 2)
                 Box.Visible = true
 
                 if teamCheck and v.TeamColor == lplr.TeamColor then
                     Box.Visible = false
-                    BoxOutline.Visible = false
                 else
                     Box.Color = Color3.new(1, 0, 0)
                 end
             else
-                BoxOutline.Visible = false
                 Box.Visible = false
             end
         else
-            BoxOutline.Visible = false
             Box.Visible = false
         end
     end)
@@ -76,7 +63,6 @@ function boxesp(v)
     v.AncestryChanged:Connect(function(_, parent)
         if not parent then
             connection:Disconnect()
-            BoxOutline:Remove()
             Box:Remove()
             boxes[v] = nil
         end
@@ -94,59 +80,94 @@ end)
 game.Players.PlayerRemoving:Connect(function(v)
     if boxes[v] then
         boxes[v][1]:Remove()
-        boxes[v][2]:Remove()
         boxes[v] = nil
     end
 end)
 
---> AIMBOT <--
+-- LINE ESP --
+
+local function lineesp(v)
+    local Tracer = Drawing.new("Line")
+    Tracer.Visible = false
+    Tracer.Color = Color3.new(1, 0, 0)
+    Tracer.Thickness = 2
+    Tracer.Transparency = 1
+
+    game:GetService("RunService").RenderStepped:Connect(function()
+        if v.Character and v.Character:FindFirstChild("HumanoidRootPart") and v ~= lplr and v.Character.Humanoid.Health > 0 then
+            local Vector, OnScreen = camera:worldToViewportPoint(v.Character.HumanoidRootPart.Position)
+
+            if OnScreen then
+                Tracer.From = Vector2.new(camera.ViewportSize.X / 2, camera.ViewportSize.Y / 1)
+                Tracer.To = Vector2.new(Vector.X, Vector.Y)
+
+                if teamCheck and v.TeamColor == lplr.TeamColor then
+                    Tracer.Visible = false
+                else
+                    Tracer.Visible = lineESP
+                end
+            else
+                Tracer.Visible = false
+            end
+        else
+            Tracer.Visible = false
+        end
+    end)
+end
+
+for _, v in pairs(game.Players:GetPlayers()) do
+    lineesp(v)
+end
+
+game.Players.PlayerAdded:Connect(function(v)
+    lineesp(v)
+end)
+
+-- AIMBOT --
 
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
-local Camera = workspace.CurrentCamera
+local Camera = game:GetService("Workspace").CurrentCamera
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
 
-local Holding = false
-
 -- AIMBOT SETTINGS
-
-_G.AimbotEnabled = false
-_G.TweenCheck = true
-_G.AimPart = "Head"
-_G.Sensitivity = 0
+local TeamCheck = false
+local AimbotEnabled = false
+local AimPart = "Head"
+local Sensitivity = 0
 
 -- FOV CIRCLE SETTINGS
 
-_G.CircleSides = 64
-_G.CircleColor = aimbotColor
-_G.CircleTransparency = 1
-_G.CircleRadius = 80
-_G.CircleFilled = false
-_G.CircleVisible = false
-_G.CircleThickness = 0
+local CircleSides = 64
+local CircleColor = aimbotColor
+local CircleTransparency = 1
+local CircleRadius = 80
+local CircleFilled = false
+local CircleVisible = false
+local CircleThickness = 0
 
 local FOVCIRCLE = Drawing.new("Circle")
 FOVCIRCLE.Position = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2)
-FOVCIRCLE.Radius = _G.CircleRadius
-FOVCIRCLE.Filled = _G.CircleFilled
-FOVCIRCLE.Color = _G.CircleColor
-FOVCIRCLE.Visible = _G.CircleVisible
-FOVCIRCLE.Transparency = _G.CircleTransparency
-FOVCIRCLE.NumSides = _G.CircleSides
-FOVCIRCLE.Thickness = _G.CircleThickness
+FOVCIRCLE.Radius = CircleRadius
+FOVCIRCLE.Filled = CircleFilled
+FOVCIRCLE.Color = CircleColor
+FOVCIRCLE.Visible = CircleVisible
+FOVCIRCLE.Transparency = CircleTransparency
+FOVCIRCLE.NumSides = CircleSides
+FOVCIRCLE.Thickness = CircleThickness
 
 local function GetClosestPlayer()
-    local MaximumDistance = _G.CircleRadius
+    local MaximumDistance = CircleRadius
     local Target = nil
 
     for _, v in pairs(Players:GetPlayers()) do
-        if v.Name ~= LocalPlayer.Name then
-            if _G.TeamCheck == true then
+        if v ~= LocalPlayer then
+            if TeamCheck then
                 if v.Team ~= LocalPlayer.Team then
-                    if v.Character and v.Character:FindFirstChild("Humanoid") and v.Character.Humanoid.Health > 0 then
-                        local ScreenPoint = Camera:WorldToScreenPoint(v.Character:FindFirstChild("HumanoidRootPart", true).Position)
+                    if v.Character and v.Character:FindFirstChild("HumanoidRootPart") and v.Character.Humanoid.Health > 0 then
+                        local ScreenPoint = Camera:WorldToScreenPoint(v.Character:FindFirstChild("HumanoidRootPart").Position)
                         local VectorDistance = (Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y) - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude
 
                         if VectorDistance < MaximumDistance then
@@ -156,8 +177,8 @@ local function GetClosestPlayer()
                     end
                 end
             else
-                if v.Character and v.Character:FindFirstChild("Humanoid") and v.Character.Humanoid.Health > 0 then
-                    local ScreenPoint = Camera:WorldToScreenPoint(v.Character:FindFirstChild("HumanoidRootPart", true).Position)
+                if v.Character and v.Character:FindFirstChild("HumanoidRootPart") and v.Character.Humanoid.Health > 0 then
+                    local ScreenPoint = Camera:WorldToScreenPoint(v.Character:FindFirstChild("HumanoidRootPart").Position)
                     local VectorDistance = (Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y) - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude
 
                     if VectorDistance < MaximumDistance then
@@ -186,22 +207,24 @@ end)
 
 RunService.RenderStepped:Connect(function()
     FOVCIRCLE.Position = UserInputService:GetMouseLocation()
-    FOVCIRCLE.Radius = _G.CircleRadius
-    FOVCIRCLE.Filled = _G.CircleFilled
-    FOVCIRCLE.Color = _G.CircleColor
-    FOVCIRCLE.Visible = _G.CircleVisible
-    FOVCIRCLE.Transparency = _G.CircleTransparency
-    FOVCIRCLE.NumSides = _G.CircleSides
-    FOVCIRCLE.Thickness = _G.CircleThickness
+    FOVCIRCLE.Radius = CircleRadius
+    FOVCIRCLE.Filled = CircleFilled
+    FOVCIRCLE.Color = CircleColor
+    FOVCIRCLE.Visible = CircleVisible
+    FOVCIRCLE.Transparency = CircleTransparency
+    FOVCIRCLE.NumSides = CircleSides
+    FOVCIRCLE.Thickness = CircleThickness
 
-    if Holding and _G.AimbotEnabled then
+    if Holding and AimbotEnabled then
         local Target = GetClosestPlayer()
-        if Target and Target.Character and Target.Character:FindFirstChild(_G.AimPart) then
-            local Tween = TweenService:Create(Camera, TweenInfo.new(_G.Sensitivity, Enum.EasingStyle.Sine, Enum.EasingDirection.Out), {CFrame = CFrame.new(Camera.CFrame.Position, Target.Character[_G.AimPart].Position)})
+        if Target and Target.Character and Target.Character:FindFirstChild(AimPart) then
+            local Tween = TweenService:Create(Camera, TweenInfo.new(Sensitivity, Enum.EasingStyle.Sine, Enum.EasingDirection.Out), { CFrame = CFrame.new(Camera.CFrame.Position, Target.Character[AimPart].Position) })
             Tween:Play()
         end
     end
 end)
+
+-- Local Player Walkspeed
 
 local player = game.Players.LocalPlayer
 local char = player.Character or player.CharacterAdded:Wait()
@@ -223,48 +246,45 @@ local section3 = tab3:section({ name = "Local Player", side = "left", size = 250
 local section4 = tab4:section({ name = "UI", side = "left", size = 75 })
 
 section1:toggle({ name = "Enabled", def = false, callback = function(boolean)
-    _G.AimbotEnabled = boolean
+    AimbotEnabled = boolean
 end })
 
-section1:dropdown({name = "Hitbox", def = "Head", options = {"Head", "HumanoidRootPart"}, callback = function(selectedOption)
-    _G.AimPart = selectedOption
-end})
+section1:dropdown({ name = "Hitbox", def = "Head", options = { "Head", "HumanoidRootPart" }, callback = function(selectedOption)
+    AimPart = selectedOption
+end })
 
 section1:toggle({ name = "FoV Visible", def = false, callback = function(boolean)
-    _G.CircleVisible = boolean
-end})
+    CircleVisible = boolean
+end })
 
 section1:slider({ name = "FoV Size", def = 80, max = 360, min = 10, rounding = true, ticking = false, measuring = "", callback = function(value)
-    _G.CircleRadius = value
-end})
+    CircleRadius = value
+end })
 
-section1:colorpicker({name = "FoV Color", cpname = "Color Picker", def = Color3.fromRGB(255, 0, 0), callback = function(color) _G.CircleColor = color end})
+section1:colorpicker({ name = "FoV Color", cpname = "Color Picker", def = Color3.fromRGB(255, 0, 0), callback = function(color) CircleColor = color end })
 
 section2:toggle({ name = "Enabled", def = false, callback = function(boolean)
     boxEnabled = boolean
-end})
+end })
+
+section2:toggle({ name = "Tracer", def = false, callback = function(boolean)
+    lineESP = boolean
+end })
 
 section2:toggle({ name = "Team Check", def = false, callback = function(boolean)
     teamCheck = boolean
-end})
+end })
 
-section2:colorpicker({name = "Box Color", cpname = "Color Picker", def = Color3.fromRGB(255, 0, 0), callback = function(color) espColor = color end})
+section2:colorpicker({ name = "Box Color", cpname = "Color Picker", def = Color3.fromRGB(255, 0, 0), callback = function(color) espColor = color end })
 
 section3:slider({ name = "Walkspeed", def = 16, max = 2450, min = 16, rounding = true, ticking = false, measuring = "", callback = function(value)
     humanoid.WalkSpeed = value
 end })
 
-section4:keybind({name = "UI Bind",def = nil,callback = function(key)
+section4:keybind({ name = "UI Bind", def = nil, callback = function(key)
     window.key = key
-end})
+end })
 
-section4:colorpicker({name = "UI Color", cpname = "Color Picker", def = Color3.fromRGB(255, 0, 0), callback = function(color) uiColor = color end})
-
-
-
-
-
-
-
+section4:colorpicker({ name = "UI Color", cpname = "Color Picker", def = Color3.fromRGB(255, 0, 0), callback = function(color) uiColor = color end })
 
 print("kaasgenieter skidded this")
